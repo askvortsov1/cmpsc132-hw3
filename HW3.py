42a99ec202765375b141b00591cf2ccde962333b
Alexander Skvortsov <askvortsov@youthneuro.org>
2019-10-19 15:30:29 -0400

    Implemented postfix conversion and added relevant tests

--- HW3.py
+++ HW3.py
@@ -1,11 +1,11 @@
 # HW3
 # Due Date: 11/03/2019, 11:59PM
-'''
-Team members:
-
-Collaboration Statement:             
-
-'''
+########################################
+#
+# Team members: Alexander Skvortsov, Vibudh Bhardwaj
+# Collaboration Statement: We completed this assignment using only this semester's course materials and official python documentation.
+#
+########################################
 import re
 
 
@@ -74,8 +74,85 @@ class Stack:
 
 # =============================================== Part II ==============================================
 class Calculator:
-    def __init__(self):
+    def __init__(self, extra_credit=False):
         self.expr = None
+        self.extra_credit = extra_credit
+
+    def _compare_precedence(self, op1, op2):
+        precedence_mapping = {
+            None: 0,
+            "(": 0,
+            "+": 1,
+            "-": 1,
+            "*": 2,
+            "/": 2,
+            "^": 3
+        }
+        return precedence_mapping.get(op1) - precedence_mapping.get(op2)
+
+    def _validate_parentheses(self, txt):
+        counter = 0
+        for i in txt:
+            if i == "(":
+                counter += 1
+            elif i == ")":
+                counter -= 1
+            if counter < 0:
+                return False
+        return counter == 0
+
+    def _preprocess_and_validate(self, txt):
+        # Validate for balanced parentheses
+        if not self._validate_parentheses(txt):
+            return None
+        # Validate against unsupported characters
+        reg_invalid = re.compile(r"""
+            [^\(\)\+\*\-\^/\d\.\s]  # Any character that's not a digit, operator, period, or whitespace
+            | (?<!\d)\.(?!\d)  # Any period without a digit on either end
+        """, re.VERBOSE)
+        if re.search(reg_invalid, txt):
+            return None
+        # Extract all relevant characters from whitespace into list
+        reg = re.compile(r"""
+            \s* \d*\.?\d+ \s*  # number wrapped by possible whitespace
+            | \s* [\(\)\+\*\-\^/] \s*  # Operator wrapped by possible whitespace
+        """ , re.VERBOSE)
+        elements = [re.sub(r'\s*', "", i) for i in re.findall(reg, txt)]  # Comprehension to remove whitespace
+        if len(elements) == 0 or elements[0] in "^*/+-" or elements[-1] in "^*/+-":
+            if elements[0] == "-" and self.isNumber(elements[1]):
+                pass
+            else:
+                return None
+        # Iterate through and validate against consequent operators, consequent numbers
+        i = 1
+        while i < len(elements):
+            # If start or open parenthese followed by minus then number, that means negative number
+            if (i == 1 or elements[i - 2] == "(") and elements[i - 1] == "-" and self.isNumber(elements[i]):
+                elements[i - 1] += elements[i]
+                elements.pop(i)
+            # Assess consequent operators
+            if elements[i - 1] in "(^*/+-" and elements[i] in "^*/+-":
+                # If extra credit and second operator is minus followed by number, merge into negative
+                if self.extra_credit and elements[i] == "-" and i < len(elements) - 1 and self.isNumber(elements[i + 1]):
+                    elements[i] += elements[i + 1]
+                    elements.pop(i + 1)
+                else:
+                    return None
+            # If extra credit, support parenthesis multiplication
+            if any([
+                elements[i - 1] == ")" and elements[i] == "(",
+                self.isNumber(elements[i - 1]) and elements[i] == "(",
+                elements[i - 1] == ")" and self.isNumber(elements[i])
+            ]):
+                if self.extra_credit:
+                    elements.insert(i, "*")
+                else:
+                    return None
+            # Consequent numbers invalid.
+            if self.isNumber(elements[i - 1]) and self.isNumber(elements[i]):
+                return None
+            i += 1
+        return elements
 
     def isNumber(self, txt):
         txt = re.sub(r'^\s+|\s+$', "", txt)
@@ -85,14 +162,54 @@ class Calculator:
         except Exception:
             return False
 
+    def isOperator(self, txt):
+        return txt.strip() in "()^*/+-"
 
     def postfix(self, txt):
         if not isinstance(txt, str) or len(txt) <= 0:
             print("Argument error in postfix")
             return None
 
+        elements = self._preprocess_and_validate(txt)
+        if elements is None:
+            return None
+
         postStack = Stack()
-        # YOUR CODE STARTS HERE
+        postList = []
+        for element in elements:
+            if self.isNumber(element):
+                postList.append(str(float(element)))
+            elif self.isOperator(element):
+                if element == "(":
+                    postStack.push(element)
+                elif element == ")":
+                    while not postStack.isEmpty():
+                        if postStack.peek() == "(":
+                            postStack.pop()
+                            break
+                        else:
+                            postList.append(postStack.pop())
+                elif self._compare_precedence(element, postStack.peek()) <= 0:
+                    while not self._compare_precedence(element, postStack.peek()) > 0:
+                        if postStack.peek() == "(":
+                            if element == ")":
+                                postStack.pop()
+                            else:
+                                pass
+                            break
+                        else:
+                            postList.append(postStack.pop())
+                    postStack.push(element)
+                else:
+                    postStack.push(element)
+            else:
+                return None
+        while not postStack.isEmpty():
+            if postStack.peek() == "(":
+                postStack.pop()
+            else:
+                postList.append(postStack.pop())
+        return " ".join(postList)
 
     @property
     def calculate(self):
@@ -100,6 +217,25 @@ class Calculator:
             print("Argument error in calculate")
             return None
 
+        postfix_str = self.postfix(self.expr)
+        if postfix_str is None:
+            return None
+
+        postList = postfix_str.split(" ")
+
+        op_mapping = {
+            "^": lambda x, y: y ** x,
+            "*": lambda x, y: y * x,
+            "/": lambda x, y: y / x,
+            "+": lambda x, y: y + x,
+            "-": lambda x, y: y - x
+        }
+
         calcStack = Stack()
 
-        # YOUR CODE STARTS HERE
+        for element in postList:
+            if self.isNumber(element):
+                calcStack.push(element)
+            elif self.isOperator(element):
+                calcStack.push(op_mapping[element](float(calcStack.pop()), float(calcStack.pop())))
+        return calcStack.pop()
--- test_hw3.py
+++ test_hw3.py
@@ -16,6 +16,32 @@ class TestStack:
 
 
 class TestCalculator:
+    def test_compare_precendence(self):
+        x = Calculator()
+        assert x._compare_precedence("^", "^") == 0
+        assert x._compare_precedence("^", "*") > 0
+        assert x._compare_precedence("*", "+") > 0
+        assert x._compare_precedence("-", "^") < 0
+
+    def test_validate_parentheses(self):
+        x = Calculator()
+        assert x._validate_parentheses("(())") is True
+        assert x._validate_parentheses("))(())()((") is False
+        assert x._validate_parentheses(")(") is False
+        assert x._validate_parentheses("(asdsadsadsadsadsa(asdsadsadds(sadsada)))") is True
+
+    def test_preprocess_and_validate(self):
+        x = Calculator(extra_credit=True)
+        assert x._preprocess_and_validate('   2 *  (  5   +   3)    ^ 2+(1  +4    ') is None
+        assert x._preprocess_and_validate('2*(5 +3)^ 2+)1  +4(    ') is None
+        assert x._preprocess_and_validate('25 +') is None
+        assert x._preprocess_and_validate('2    5') is None
+        assert x._preprocess_and_validate('2 *    5   +   3    ^ -2       +1  +4') == ["2", "*", "5", "+", "3", "^", "-2", "+", "1", "+", "4"]
+        assert x._preprocess_and_validate("2(4)") == ["2", "*", "(", "4", ")"]
+        x.extra_credit = False
+        assert x._preprocess_and_validate('2 *    5   +   3    ^ -2       +1  +4') is None
+        assert x._preprocess_and_validate("2(4)") is None
+
     def test_is_number(self):
         x = Calculator()
         assert x.isNumber('hello') is False
@@ -26,22 +52,25 @@ class TestCalculator:
 
     def test_postfix_doctest(self):
         x = Calculator()
-        assert x.postfix(' 2 ^        4') == '2.0 4.0 ^'
-        assert x.postfix('2') == '2.0'
-        assert x.postfix('2.1*5+3^2+1+4.45') == '2.1 5.0 * 3.0 2.0 ^ + 1.0 + 4.45 +'
-        assert x.postfix('    2 *       5.34        +       3      ^ 2    + 1+4   ') == '2.0 5.34 * 3.0 2.0 ^ + 1.0 + 4.0 +'
-        assert x.postfix(' 2.1 *      5   +   3    ^ 2+ 1  +     4') == '2.1 5.0 * 3.0 2.0 ^ + 1.0 + 4.0 +'
-        assert x.postfix('(2.5)') == '2.5'
-        assert x.postfix('((2))') == '2.0'
-        assert x.postfix('     -2 *  ((  5   +   3)    ^ 2+(1  +4))    ') == '-2.0 5.0 3.0 + 2.0 ^ 1.0 4.0 + + *'
-        assert x.postfix('  (   2 *  ((  5   +   3)    ^ 2+(1  +4)))    ') == '2.0 5.0 3.0 + 2.0 ^ 1.0 4.0 + + *'
-        assert x.postfix('  ((   2 *  ((  5   +   3)    ^ 2+(1  +4))))    ') == '2.0 5.0 3.0 + 2.0 ^ 1.0 4.0 + + *'
-        assert x.postfix('   2 *  (  5   +   3)    ^ 2+(1  +4)    ') == '2.0 5.0 3.0 + 2.0 ^ * 1.0 4.0 + +'
-        assert x.postfix('2 *    5   +   3    ^ -2       +1  +4') == 'error message'
-        assert x.postfix('2    5') == 'error message'
-        assert x.postfix('25 +') == 'error message'
-        assert x.postfix('   2 *  (  5   +   3)    ^ 2+(1  +4    ') == 'error message'
-        assert x.postfix('2*(5 +3)^ 2+)1  +4(    ') == 'error message'
+        # assert x.postfix(' 2 ^        4') == '2.0 4.0 ^'
+        # assert x.postfix('2') == '2.0'
+        # assert x.postfix('2.1*5+3^2+1+4.45') == '2.1 5.0 * 3.0 2.0 ^ + 1.0 + 4.45 +'
+        # assert x.postfix('    2 *       5.34        +       3      ^ 2    + 1+4   ') == '2.0 5.34 * 3.0 2.0 ^ + 1.0 + 4.0 +'
+        # assert x.postfix(' 2.1 *      5   +   3    ^ 2+ 1  +     4') == '2.1 5.0 * 3.0 2.0 ^ + 1.0 + 4.0 +'
+        # assert x.postfix('(2.5)') == '2.5'
+        # assert x.postfix('((2))') == '2.0'
+        # assert x.postfix('     -2 *  ((  5   +   3)    ^ 2+(1  +4))    ') == '-2.0 5.0 3.0 + 2.0 ^ 1.0 4.0 + + *'
+        # assert x.postfix('  (   2 *  ((  5   +   3)    ^ 2+(1  +4)))    ') == '2.0 5.0 3.0 + 2.0 ^ 1.0 4.0 + + *'
+        # assert x.postfix('  ((   2 *  ((  5   +   3)    ^ 2+(1  +4))))    ') == '2.0 5.0 3.0 + 2.0 ^ 1.0 4.0 + + *'
+        # assert x.postfix('   2 *  (  5   +   3)    ^ 2+(1  +4)    ') == '2.0 5.0 3.0 + 2.0 ^ * 1.0 4.0 + +'
+        # assert x.postfix(' 23 / 12 - 223 +      5.25 * 4    *      3423') == '23.0 12.0 / 223.0 - 5.25 4.0 * 3423.0 * +'
+        assert x.postfix('2 *    5   +   3    ^ -2       +1  +4') is None
+        assert x.postfix('2    5') is None
+        assert x.postfix('25 +') is None
+        assert x.postfix('   2 *  (  5   +   3)    ^ 2+(1  +4    ') is None
+        assert x.postfix('2*(5 +3)^ 2+)1  +4(    ') is None
+        x.extra_credit = True
+        assert x.postfix('(2.5)(1)(5)') == '2.5 1.0 * 5.0 *'
 
     def test_calculate_doctest(self):
         x = Calculator()
@@ -64,12 +93,19 @@ class TestCalculator:
         x.expr = '2.5 + 3 * ( 2 +(3.0) *(5^2 - 2*3^(2) ) *(4) ) * ( 2 /8 + 2*( 3 - 1/ 3) ) - 2/ 3^2'
         assert x.calculate == 1442.7777777777778
         x.expr = "4++ 3 +2"
-        assert x.calculate == 'error message'
+        assert x.calculate is None
         x.expr = "4    3 +2"
-        assert x.calculate == 'error message'
+        assert x.calculate is None
         x.expr = '(2)*10 - 3*(2 - 3*2)) '
-        assert x.calculate == 'error message'
+        assert x.calculate is None
         x.expr = '(2)*10 - 3*/(2 - 3*2) '
-        assert x.calculate == 'error message'
+        assert x.calculate is None
         x.expr = ')2(*10 - 3*(2 - 3*2) '
-        assert x.calculate == 'error message'
+        assert x.calculate is None
+        x.extra_credit = True
+        x.expr = '(2.5)(1)(5)'
+        assert x.calculate == 12.5
+        x.expr = '3(6)'
+        assert x.calculate == 18
+        x.expr = '(6)3'
+        assert x.calculate == 18
